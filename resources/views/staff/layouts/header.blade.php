b35a432c40fc4acb6490b4de2694556c776b9f65
MUHAMMAD ZIKRI BIN KASHIM <zikrimzk@gmail.com>
2025-08-20 23:15:47 +0800

    Minor Change : PSM Part 10D
    Deleted the dropdown menu for switching between dark, light, and default themes in the staff header layout. This simplifies the header UI and removes theme selection options for staff users.

--- resources/views/staff/layouts/header.blade.php
+++ resources/views/staff/layouts/header.blade.php
@@ -24,34 +24,6 @@
                 <li class="dropdown pc-h-item">
                     <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
                         role="button" aria-haspopup="false" aria-expanded="false">
-                        <svg class="pc-icon">
-                            <use xlink:href="#custom-sun-1"></use>
-                        </svg>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end pc-h-dropdown">
-                        <a href="#!" class="dropdown-item" onclick="layout_change('dark')">
-                            <svg class="pc-icon">
-                                <use xlink:href="#custom-moon"></use>
-                            </svg>
-                            <span>Dark</span>
-                        </a>
-                        <a href="#!" class="dropdown-item" onclick="layout_change('light')">
-                            <svg class="pc-icon">
-                                <use xlink:href="#custom-sun-1"></use>
-                            </svg>
-                            <span>Light</span>
-                        </a>
-                        <a href="#!" class="dropdown-item" onclick="layout_change_default()">
-                            <svg class="pc-icon">
-                                <use xlink:href="#custom-setting-2"></use>
-                            </svg>
-                            <span>Default</span>
-                        </a>
-                    </div>
-                </li>
-                <li class="dropdown pc-h-item">
-                    <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
-                        role="button" aria-haspopup="false" aria-expanded="false">
                         <i class="fas fa-user-circle"></i>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end pc-h-dropdown">
